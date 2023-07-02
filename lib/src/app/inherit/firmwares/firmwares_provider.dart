195f8e6f4d27644c595916a3842fa38a45668ea2
Azat Mironov <65981689+Keddnyo@users.noreply.github.com>
2023-07-02 13:27:43 +0300

    Update firmwares_provider.dart

--- lib/src/app/inherit/firmwares/firmwares_provider.dart
+++ lib/src/app/inherit/firmwares/firmwares_provider.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:miyolla/src/app/inherit/firmwares/firmwares_view_model.dart';
 
 class FirmwaresProvider extends InheritedNotifier {
-  const FirmwaresProvider({super.key, required super.child});
+  const FirmwaresProvider(
+      {Key? key, required this.model, required Widget child})
+      : super(key: key, notifier: model, child: child);
+
+  final FirmwaresViewModel model;
+
+  static FirmwaresViewModel? of(BuildContext context) =>
+      context.dependOnInheritedWidgetOfExactType<FirmwaresProvider>()?.model;
 }
